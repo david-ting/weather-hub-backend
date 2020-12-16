92193c9d8772a74e7d6ee8a5e6f91f86b6d96c52
david-ting <davidting159@gmail.com>
2020-12-16 11:30:37 +0800

    change validate_city output

--- index.js
+++ index.js
@@ -22,6 +22,10 @@ const pool = new Pool({
   },
 });
 
+const escapeString = (str) => {
+  return str.replace(/'/g, "''").replace(/%/g, "\\%").replace(/_/g, "\\_");
+};
+
 app.get("/forecasts/:options/:part", async (req, res) => {
   const { options, part } = req.params;
   const url = `https://api.openweathermap.org/data/2.5/onecall?${options}&exclude=${part}&units=metric&appid=${API_KEY}`;
@@ -34,16 +38,13 @@ app.get("/forecasts/:options/:part", async (req, res) => {
       throw new Error();
     }
   } catch (err) {
-    console.log(err);
+    console.error(err);
     res.status(500).end();
   }
 });
 
 app.get("/validate_countryCode/:code", async (req, res) => {
-  const code = req.params.code
-    .replace(/'/g, "''")
-    .replace(/%/g, "\\%")
-    .replace(/_/g, "\\_");
+  const code = escapeString(req.params.code);
 
   try {
     const result = await pool.query(
@@ -60,38 +61,27 @@ app.get("/validate_countryCode/:code", async (req, res) => {
         break;
     }
   } catch (err) {
-    console.log(err);
+    console.error(err);
     res.status(500).end();
   }
 });
 
 app.get("/validate_city/:countryCode/:name", async (req, res) => {
   try {
-    const countryCode = req.params.countryCode
-      .replace(/'/g, "''")
-      .replace(/%/g, "\\%")
-      .replace(/_/g, "\\_");
+    const countryCode = escapeString(req.params.countryCode);
+    const name = escapeString(req.params.name);
 
-    const name = req.params.name
-      .replace(/'/g, "''")
-      .replace(/%/g, "\\%")
-      .replace(/_/g, "\\_");
-
-    const result = await pool.query(
-      `SELECT COUNT(1) FROM cities WHERE name ILIKE '${name}' AND country ILIKE '${countryCode}'`
+    const results = await pool.query(
+      `SELECT * FROM cities WHERE name ILIKE '${name}' AND country ILIKE '${countryCode}'`
     );
-    switch (result.rows[0].count) {
-      case "0":
-        res.json(false);
-        break;
-      case "1":
-        res.json(true);
-        break;
-      default:
-        break;
+
+    if (results.rows[0]) {
+      res.json({ found: true, result: results.rows[0] });
+    } else {
+      res.json({ found: false });
     }
   } catch (err) {
-    console.log(err);
+    console.error(err);
     res.status(500).end();
   }
 });
@@ -100,16 +90,9 @@ app.get("/validate_city/:countryCode/:name", async (req, res) => {
 app.get("/get_countries/:type/:query", async (req, res) => {
   const exact = req.query.exact;
   const type = req.params.type;
-  const query = req.params.query
-    .replace(/'/g, "''")
-    .replace(/%/g, "\\%")
-    .replace(/_/g, "\\_");
+  const query = escapeString(req.params.query);
+
   try {
-    console.log(
-      `SELECT * FROM countries WHERE ${type} ILIKE '${query}${
-        exact === "true" ? "" : "%"
-      }'`
-    );
     const result = await pool.query(
       `SELECT * FROM countries WHERE ${type} ILIKE '${query}${
         exact === "true" ? "" : "%"
@@ -117,7 +100,7 @@ app.get("/get_countries/:type/:query", async (req, res) => {
     );
     res.json(result.rows);
   } catch (err) {
-    console.log(err);
+    console.error(err);
     res.status(500).end();
   }
 });
@@ -125,10 +108,8 @@ app.get("/get_countries/:type/:query", async (req, res) => {
 app.get("/get_allCities/:countryCode", async (req, res) => {
   const offset = parseInt(req.query.offset);
   const limit = parseInt(req.query.limit);
-  const code = req.params.countryCode
-    .replace(/'/g, "''")
-    .replace(/%/g, "\\%")
-    .replace(/_/g, "\\_");
+  const code = escapeString(req.params.countryCode);
+
   try {
     const results = await pool.query(
       `SELECT *, count(*) OVER() AS total
@@ -147,7 +128,7 @@ app.get("/get_allCities/:countryCode", async (req, res) => {
       }),
     });
   } catch (err) {
-    console.log(err);
+    console.error(err);
     res.status(500).end();
   }
 });
@@ -155,16 +136,8 @@ app.get("/get_allCities/:countryCode", async (req, res) => {
 app.get("/get_cities/:countryCode/:name", async (req, res) => {
   const offset = parseInt(req.query.offset);
   const limit = parseInt(req.query.limit);
-  const code = req.params.countryCode
-    .replace(/'/g, "''")
-    .replace(/%/g, "\\%")
-    .replace(/_/g, "\\_");
-  const name = req.params.name
-    .replace(/'/g, "''")
-    .replace(/%/g, "\\%")
-    .replace(/_/g, "\\_");
-
-  console.log(name);
+  const code = escapeString(req.params.countryCode);
+  const name = escapeString(req.params.name);
 
   try {
     const results = await pool.query(
@@ -184,7 +157,7 @@ app.get("/get_cities/:countryCode/:name", async (req, res) => {
       }),
     });
   } catch (err) {
-    console.log(err);
+    console.error(err);
     res.status(500).end();
   }
 });
